3ef65fe90436cf8dfb48fe09ead14bf5b04ad40b
Dr. Arnold Sykosch <ars@sonnenglanz.local>
2026-07-27 17:41:22 +0200

    chore: sync shared files from central repo

--- conf.py
+++ conf.py
@@ -9,6 +9,10 @@ extensions = [
     'sphinx_rtd_theme',
 ]
 
+# we do have duplicate section headings by design
+suppress_warnings = ['autosectionlabel.*']
+
 html_theme = 'sphinx_rtd_theme'
 master_doc = 'content/index'
 language = 'en'
+exclude_patterns = ['_build', '.venv', 'Thumbs.db', '.DS_Store']
