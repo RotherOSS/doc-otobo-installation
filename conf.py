a8d1a2ff35d9e3089eba2c2eff5fe92f2e7800f3
Dr. Arnold Sykosch <ars@sonnenglanz.local>
2026-07-31 17:00:06 +0200

    chore: sync shared files from central repo

--- conf.py
+++ conf.py
@@ -16,3 +16,14 @@ html_theme = 'sphinx_rtd_theme'
 master_doc = 'content/index'
 language = 'en'
 exclude_patterns = ['_build', '.venv', 'Thumbs.db', '.DS_Store']
+
+linkcheck_anchors_ignore_for_url = [
+    r'https://github\.com/.*'
+]
+linkcheck_allowed_redirects = {
+    r'https://otobo\.de': r'https://otobo\.io/de/.*',
+    r'https://otobo\.io': r'https://otobo\.io/de/.*',
+    r'https://tools\.ietf\.org/html/rfc.*': r'https://datatracker\.ietf\.org/doc/html/rfc.*'
+}
+linkcheck_anchors = False
+
